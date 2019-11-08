6b796ef1958fd13974d04c79494d56b4f3138076
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2019-11-08 11:09:35 +0100

    New task

--- app/src/androidTest/java/com/yurich/lastfmapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/yurich/lastfmapplication/ExampleInstrumentedTest.kt
@@ -8,11 +8,10 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+// TODO
+// Espresso test
+// 1. Open Search
+// 2. Check if any element on the screen exists
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
